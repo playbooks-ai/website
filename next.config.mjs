d4409f3c1787d02515fd42aa161ca19745630dd3
Amol Kelkar <kelkar.amol@gmail.com>
2025-07-20 01:07:14 -0700

    npm update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  distDir: process.env.NODE_ENV === "production" ? ".next-prod" : ".next",
   typescript: {
     ignoreBuildErrors: true
   }
